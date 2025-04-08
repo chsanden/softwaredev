e7ac601220c7e7a396fba202002d1d7d57f4423a
chsanden <c.sanden@outlook.com>
2025-04-08 19:54:16 +0200

    Add files via upload
    Second assignment of semester 2 - part 1

---
+++ assignment_2_1/main.cpp
@@ -0,0 +1,15 @@
+
+#include "func.h"
+
+int main()
+{
+    Display display;
+
+    display.Customer();
+    display.Product();
+    display.Order();
+    display.GrossIncome();
+    display.CustomerSpent();
+
+
+}
